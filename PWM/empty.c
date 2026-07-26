43036775a56c37e56a0476484bd726e6512c4762
Qhw126 <292963273+Qhw126@users.noreply.github.com>
2026-07-26 22:04:16 +0800

    first

--- PWM/empty.c
+++ PWM/empty.c
@@ -32,23 +32,9 @@
 
 #include "ti_msp_dl_config.h"
 
-void delay_ms(unsigned int ms)
-{
-    unsigned int i, j;
-    // 下面的嵌套循环的次数是根据主控频率和编译器生成的指令周期大致计算出来的，
-    // 需要通过实际测试调整来达到所需的延时。
-    for (i = 0; i < ms; i++)
-    {
-        for (j = 0; j < 8000; j++)
-        {
-            // 仅执行一个足够简单以致于可以预测其执行时间的操作
-            __asm__("nop"); // "nop" 代表“无操作”，在大多数架构中，这会消耗一个或几个时钟周期
-        }
-    }
-}
 int main(void)
 {
-      int i = 0;
+    int i = 0;
     SYSCFG_DL_init();
     while (1)
     {
